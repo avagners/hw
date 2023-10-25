c67f72fafb00eba2002669688dae875ff09c3e5b
Vagner Alexander <onemybox@gmail.com>
2023-10-25 13:49:13 +0300

    6: создал АТД AbsStack

---
+++ 6_minimize_change_steps/source/example_1/Stack.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+
+# АТД Стека
+class AbsStack(ABC, Generic[T]):
+
+    # Конструктор
+    # постусловие: создан новый пустой стек
+    def __init__(self): ...
+
+    # Команды:
+    # постусловие: в стек добавлено новое значение
+    @abstractmethod
+    def push(self, value: T) -> None: ...
+
+    # предусловие: стек не пустой
+    # постусловие: из стека удалён верхний элемент
+    @abstractmethod
+    def pop(self) -> None: ...
+
+    # Запросы:
+    # предусловие: стек не пустой
+    @abstractmethod
+    def peek(self) -> T: ...  # возвращает верхний элемент списка
+    @abstractmethod
+    def size(self) -> int: ...
