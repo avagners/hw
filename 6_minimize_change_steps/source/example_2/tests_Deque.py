a36fc826595e6a1a3da39719ee142b97b7677d87
Vagner Alexander <onemybox@gmail.com>
2023-10-26 19:29:56 +0300

    добавил тесты метода addTail

--- 6_minimize_change_steps/source/example_2/tests_Deque.py
+++ 6_minimize_change_steps/source/example_2/tests_Deque.py
@@ -20,6 +20,17 @@ class TestDeque(unittest.TestCase):
         self.assertEqual(len(self.s_deque.deque), len_queue + 2)
         self.assertEqual(self.s_deque.deque[0], item2)
 
+    def test_empty_add_tail(self):
+        len_queue = len(self.s_deque.deque)
+        item1 = random.randint(0, 100)
+        self.s_deque.addTail(item1)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 1)
+        self.assertEqual(self.s_deque.deque[-1], item1)
+        item2 = random.randint(0, 100)
+        self.s_deque.addTail(item2)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 2)
+        self.assertEqual(self.s_deque.deque[-1], item2)
+
 
 if __name__ == '__main__':
     unittest.main()
