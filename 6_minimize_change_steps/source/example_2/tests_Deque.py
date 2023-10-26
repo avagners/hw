e5129fe9762c0d302a1980b4cd801f88bdf2eb00
Vagner Alexander <onemybox@gmail.com>
2023-10-26 19:25:19 +0300

    добавил тест метода addFront

---
+++ 6_minimize_change_steps/source/example_2/tests_Deque.py
@@ -0,0 +1,25 @@
+import random
+import unittest
+
+from Deque import Deque
+
+
+class TestDeque(unittest.TestCase):
+
+    def setUp(self):
+        self.s_deque = Deque()
+
+    def test_add_front(self):
+        len_queue = len(self.s_deque.deque)
+        item1 = random.randint(0, 100)
+        self.s_deque.addFront(item1)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 1)
+        self.assertEqual(self.s_deque.deque[0], item1)
+        item2 = random.randint(0, 100)
+        self.s_deque.addFront(item2)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 2)
+        self.assertEqual(self.s_deque.deque[0], item2)
+
+
+if __name__ == '__main__':
+    unittest.main()
