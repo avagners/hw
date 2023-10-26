1adc9725f75ebe9089af9a144be73dff04c2eb2f
Vagner Alexander <onemybox@gmail.com>
2023-10-26 19:47:56 +0300

    добавил тесты метода removeFront

--- 6_minimize_change_steps/source/example_2/tests_Deque.py
+++ 6_minimize_change_steps/source/example_2/tests_Deque.py
@@ -20,7 +20,7 @@ class TestDeque(unittest.TestCase):
         self.assertEqual(len(self.s_deque.deque), len_queue + 2)
         self.assertEqual(self.s_deque.deque[0], item2)
 
-    def test_empty_add_tail(self):
+    def test_add_tail(self):
         len_queue = len(self.s_deque.deque)
         item1 = random.randint(0, 100)
         self.s_deque.addTail(item1)
@@ -30,6 +30,22 @@ class TestDeque(unittest.TestCase):
         self.s_deque.addTail(item2)
         self.assertEqual(len(self.s_deque.deque), len_queue + 2)
         self.assertEqual(self.s_deque.deque[-1], item2)
+
+    def test_remove_front(self):
+        len_queue = len(self.s_deque.deque)
+        item1 = random.randint(0, 100)
+        self.s_deque.addFront(item1)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 1)
+        self.assertEqual(self.s_deque.deque[0], item1)
+        result_remove = self.s_deque.removeFront()
+        self.assertEqual(len(self.s_deque.deque), len_queue)
+        self.assertIsNotNone(result_remove)
+        self.assertEqual(result_remove, item1)
+        self.assertListEqual(self.s_deque.deque, [])
+        result_remove = self.s_deque.removeFront()
+        self.assertEqual(len(self.s_deque.deque), len_queue)
+        self.assertIsNone(result_remove)
+        self.assertListEqual(self.s_deque.deque, [])
 
 
 if __name__ == '__main__':
