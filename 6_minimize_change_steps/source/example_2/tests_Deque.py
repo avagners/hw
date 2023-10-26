5ce2729c976a3aae14b53a316e20e595271d0dda
Vagner Alexander <onemybox@gmail.com>
2023-10-26 19:56:42 +0300

    добавил тесты для метода removeTail

--- 6_minimize_change_steps/source/example_2/tests_Deque.py
+++ 6_minimize_change_steps/source/example_2/tests_Deque.py
@@ -47,6 +47,22 @@ class TestDeque(unittest.TestCase):
         self.assertIsNone(result_remove)
         self.assertListEqual(self.s_deque.deque, [])
 
+    def test_remove_tail(self):
+        len_queue = len(self.s_deque.deque)
+        item1 = random.randint(0, 100)
+        self.s_deque.addFront(item1)
+        self.assertEqual(len(self.s_deque.deque), len_queue + 1)
+        self.assertEqual(self.s_deque.deque[0], item1)
+        result_remove = self.s_deque.removeTail()
+        self.assertEqual(len(self.s_deque.deque), len_queue)
+        self.assertIsNotNone(result_remove)
+        self.assertEqual(result_remove, item1)
+        self.assertListEqual(self.s_deque.deque, [])
+        result_remove = self.s_deque.removeTail()
+        self.assertEqual(len(self.s_deque.deque), len_queue)
+        self.assertIsNone(result_remove)
+        self.assertListEqual(self.s_deque.deque, [])
+
 
 if __name__ == '__main__':
     unittest.main()
